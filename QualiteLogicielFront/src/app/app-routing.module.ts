d9118a7e153404df591f18c85cc6ca694ba6955b
isatiss73 <q.isatis@gmail.com>
2024-02-09 20:12:08 +0100

    route nouveau-materiel
    ajout de la route vers nouveau-materiel

--- QualiteLogicielFront/src/app/app-routing.module.ts
+++ QualiteLogicielFront/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AccueilComponent} from "./pages/accueil/accueil.component";
+import {NouveauMaterielComponent} from "./pages/nouveau-materiel/nouveau-materiel.component";
 import {AuthGuard} from "./auth.guard";
 
 const routes: Routes = [
   { path: "accueil", component: AccueilComponent, canActivate: [AuthGuard] },
+  { path: "nouveau-materiel", component: NouveauMaterielComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'accueil', pathMatch: 'full' },
 ];
 
